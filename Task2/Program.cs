6edabfaa811a6912f6650051328ff1aff25a0517
Ria.x <ria.x@mail.ru>
2024-01-06 15:37:27 +0300

    added quotient2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -18,4 +18,8 @@ Console.WriteLine(product2);
 Console.WriteLine(product3);
 
 int quotient = i1 / i2;
-Console.WriteLine(quotient);quotient
+Console.WriteLine(quotient);
+
+d1 = 6;
+double quotient2 = i1 /d1;
+Console.WriteLine(quotient2);
